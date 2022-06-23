5495d3d894124acc6fbd8bb2d97e3c41cfcfcf46
anniehau <anniehaurani@gmail.com>
2022-06-23 16:49:42 -0300

    feat: Login, creates title component

---
+++ front-end/src/components/Text/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <>
+      <h1>夜 Calendar</h1>
+    </>
+  )
+}
+
+export default Title;
--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import LoginForm from '../../components/Forms/LoginForm/LoginForm';
+import Title from '../../components/Text/Title';
 
 function Login() {
   return (
     <>
+      <Title />
       <LoginForm />
     </>
   )
