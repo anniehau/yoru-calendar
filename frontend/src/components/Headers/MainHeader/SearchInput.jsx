79a01483715fe003e42ed300f06cf93dc6d38ef8
anniehau <anniehaurani@gmail.com>
2022-06-30 02:18:36 -0300

    feat: creates search input component

---
+++ frontend/src/components/Headers/MainHeader/SearchInput.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SearchInput() {
+	return (
+		<>
+			<input
+				className="search__bar"
+				type="text"
+				placeholder="Search task..."
+			/>
+		</>
+	);
+}
+
+export default SearchInput;
