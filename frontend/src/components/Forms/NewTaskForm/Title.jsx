8d6b151a7f1b680a0957c4e8df59af7946a1727f
anniehau <anniehaurani@gmail.com>
2022-07-03 23:36:06 -0300

    feat: changes datetime input css

--- frontend/src/components/Forms/NewTaskForm/Title.jsx
+++ frontend/src/components/Forms/NewTaskForm/Title.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { BsCalendarPlusFill } from 'react-icons/bs';
 
 function Title() {
 	return (
 		<h1 className="newTask__pageTitle">
+			<BsCalendarPlusFill />
+			{' '}
 			New Task
 		</h1>
 	);
